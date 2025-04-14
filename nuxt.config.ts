fab6a67072b05edd6085e9badd1466c4d3116a3f
Matthias Zepper <MatthiasZepper@users.noreply.github.com>
2025-04-14 20:42:48 +0200

    Add UnoCSS

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,6 +13,7 @@ export default defineNuxtConfig({
     'nuxt-auth-utils',
     '@pinia/nuxt',
     '@pinia/colada-nuxt',
+    '@unocss/nuxt',
   ],
 
   // Enable Nuxt Developer Tools
---
+++ uno.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'unocss'
+import presetWind4 from '@unocss/preset-wind4'
+
+export default defineConfig({
+  // ...UnoCSS options
+  presets: [
+    presetWind4(),
+  ],
+})
