b549aea5e4ef5477d3127a3b780c3a6d517ae9b6
Matthias Zepper <MatthiasZepper@users.noreply.github.com>
2026-01-29 15:58:30 +0100

    chore:Dependency upgrade.

--- app/pages/profile/index.vue
+++ app/pages/profile/index.vue
@@ -7,10 +7,6 @@ definePageMeta({
 <template>
   <main class="mx-auto max-w-lg px-4 py-8 lg:px-8 sm:px-6 flex flex-col gap-4">
     Logged in!
-    <DialogBarcodeReader
-      type="LiveStream"
-      :reader-types="['code_128_reader']"
-    />
     <DialogZxingReader />
   </main>
 </template>
