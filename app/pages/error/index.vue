e0af00c3c0ddaa7610645240610c9f50f41aa5cd
Matthias Zepper <MatthiasZepper@users.noreply.github.com>
2026-02-25 16:30:35 +0100

    chore:error Improved error message and display.

--- app/pages/error/index.vue
+++ app/pages/error/index.vue
@@ -11,22 +11,23 @@ definePageMeta({
         <LogoFirn class="h-auto w-full max-w-[200px] min-h-[50px]" />
       </div>
 
-      <div class="border border-base rounded-md p-4 text-center text-sm flex flex-col items-center space-y-4">
+      <div class="border-1 border-primary-600 dark:border-primary-400 rounded-md p-4 text-center text-sm flex flex-col items-center space-y-4 bg-primary-50 dark:bg-primary-700">
         <NAvatar
           icon
-          label="i-lucide-badge-alert"
-          size="sm:3xl md:4xl lg:5xl"
-          class="border-2 border-primary-600 dark:border-primary-400
-            rounded-full
-            text-primary-600 dark:text-primary-400
-            bg-primary-600/30 dark:bg-primary-400/30"
+          src="/ui/firn-error_carrot-updating-or-repairing_by-David-Revoy.webp" 
+          alt="'Carrot updating or repairing' by David Revoy, licensed under Creative Commons Attribution 4.0."
+          size="sm:3xl md:4xl lg:6xl"
+          class="border-1 border-primary-600 dark:border-primary-400 shadow-sm shadow-primary-200 dark:shadow-primary-400
+            rounded-full dark:opacity-75"
         />
-        <h3 class="text-lg font-semibold">
-          An error has occurred with Firn.
-        </h3>
         <p class="text-muted">
-          Sorry, something went wrong. Please try again later.
+          <NLink href="https://www.peppercarrot.com/en/viewer/misc__2016-04-13_carrot-updating-or-repairing_by-David-Revoy.html" target="_blank" external>
+            <em>'Carrot updating or repairing'</em> by David Revoy,<br><NLink href="https://creativecommons.org/licenses/by/4.0/" target="_blank" external>Creative Commons Attribution 4.0</NLink> licensed.
+          </NLink>
         </p>
+        <h1 class="scroll-m-20 text-center font-display font-semibold text-primary-600 dark:text-primary-400 tracking-tight text-display text-2xl lg:text-3xl">
+          Sorry, we have technical difficulties at the moment.
+        </h1>
 
         <NButton
           btn="ghost"
