5d40a851979c57200546980106088c47ab4e42e2
Matthias Zepper <MatthiasZepper@users.noreply.github.com>
2026-02-25 15:39:00 +0100

    feat:projects Project detail view

---
+++ app/pages/projects/details/[projectid].vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+definePageMeta({
+  layout: 'private',
+  validate: (route) => {
+    return /^P[0-9]+$/.test(route.params.projectid as string)
+  }
+})
+
+// Get the project ID from the URL: app/pages/projects/details/[projectid].vue
+const route = useRoute()
+const projectId = computed(() => route.params.projectid as string)
+
+
+</script>
+
+<template>
+  <div>
+    <p>Project ID: {{ projectId }}</p>
+  </div>
+</template>
