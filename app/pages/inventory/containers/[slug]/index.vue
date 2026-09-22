1eb00575e2a791515f479b7b8ea33d2f97dadd93
Matthias Zepper <MatthiasZepper@users.noreply.github.com>
2026-09-22 18:36:25 +0200

    feat:inventory Add missing routes for inventory slugs.

---
+++ app/pages/inventory/containers/[slug]/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+// Redirect to the inventory containers list
+navigateTo('/inventory/containers', { replace: true })
+</script>
+
+<template>
+  <div>
+    <h1>Nothing to see here.</h1>
+  </div>
+</template>
